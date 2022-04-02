4012f0501f77b710ddda3a8a402a469b3f18e8bf
James Andrews <jandrews7348@floridapoly.edu>
2022-04-02 10:14:43 -0400

    Add files via upload

--- boat/code.py
+++ boat/code.py
@@ -1,4 +1,4 @@
-# Transmitter Base-Station
+# Receiver Boat
 # by James Andrews <jandrews7348@floridapoly.edu
 import time
 import busio
@@ -8,13 +8,11 @@ from adafruit_tinylora.adafruit_tinylora import TTN, TinyLoRa
 from digitalio import DigitalInOut, Direction, Pull
 import adafruit_rfm9x
 
-# Board Pinouts
-OP1 = digitalio.DigitalInOut(board.D11)
-OP2 = digitalio.DigitalInOut(board.D12)
-led = digitalio.DigitalInOut(board.D13)
+# Custom Libs
+import boat
 
-OP1.direction = digitalio.Direction.OUTPUT
-OP2.direction = digitalio.Direction.OUTPUT
+# Pinouts
+led = digitalio.DigitalInOut(board.D13)
 led.direction = digitalio.Direction.OUTPUT
 
 # Create library object using our bus SPI port for radio
@@ -24,52 +22,19 @@ irq = digitalio.DigitalInOut(board.RFM9X_D0)
 rst = digitalio.DigitalInOut(board.RFM9X_RST)
 rfm9x = adafruit_rfm9x.RFM9x(spi, cs, rst, 915.0)
 
-# Light Handler Function
-def SetLight(status):
-    if status == "No Link":
-        OP1.value = False
-        OP2.value = False
-    elif status == "Autonomous Mode":
-        OP1.value = True
-        OP2.value = True
-    elif status == "Secondary Mode":
-        OP1.value = True
-        OP2.value = True
-    elif status == "Manual Mode":
-        OP1.value = True
-        OP2.value = False
-    elif status == "Emergency Stop":
-        OP1.value = False
-        OP2.value = False
-    else:
-        OP1.value = False
-        OP2.value = False
-
-while True:
+ESTOP = False #ToDo: Controlled by EStop sw
+while not ESTOP:
     led.value = True
     # Receive Message over LoRa
     data = rfm9x.receive(timeout=5)
     led.value = False
-    if str(data) == 'None':
-        status = "No Link"
 
-    elif bytes(data) == b'A':
-        status = "Autonomous Mode"
-
-    elif bytes(data) == b'S':
-        status = "Secondary Mode"
-
-    elif bytes(data) == b'M':
-        status = "Manual Mode"
-
-    elif bytes(data) == b'E':
-        status = "Emergency Stop"
-
-    else:
-        print(bytes(data))
-        status = "Unknown"
-
-    print(status)
-    SetLight(status)
-    # Send Status over LoRa
-    rfm9x.send(status)
+    report = boat.ProcessCmd(data)
+    print(report)
+    boat.SetLight(report[0])
+    # Send Report back over LoRa
+    rfm9x.send(report)
+else:
+    print("ESTOP ACTIVE!")
+    report[0] = "E"
+    report[2] = "T"
